34ca46eb67d31f4f0d9f66b520224187d362334e
seneca <senecamartin11@gmail.com>
2021-07-07 22:53:00 +0200

    Navegación hacia el Home

--- Marvel-App/Features/Splash/Controllers/SplashViewController.swift
+++ Marvel-App/Features/Splash/Controllers/SplashViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class SplashViewContrller: UIViewController {
     
+    
     // MARK: - IBOutlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
@@ -21,6 +22,8 @@ class SplashViewContrller: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        navigationController?.isNavigationBarHidden = true
+        
         activityIndicator.startAnimating()
         loadData()
     }
@@ -28,13 +31,30 @@ class SplashViewContrller: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
+        navigationController?.isNavigationBarHidden = false
+        
         activityIndicator.stopAnimating()
     }
     
     // MARK: - Private methhods
     private func loadData() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) { [weak self] in
             print("Load data completed")
+            
+      //     Navegación con SEGUE
+            
+     //      self?.performSegue(withIdentifier: "SEGUE_FROM_SPLASH_TO_HOME",
+     //                         sender: nil)
+            
+     
+    //      Navegación manual tener en cuenta setViewController para vaciar la pila de navegación. Con pushView... se haria navegación normal
+            
+            let storyboardHome = UIStoryboard(name: "Home",
+                                              bundle: nil)
+            if let destination = storyboardHome.instantiateInitialViewController() {
+                self?.navigationController?.setViewControllers([destination],
+                                                               animated: true)
+            }
         }
     }
 }
