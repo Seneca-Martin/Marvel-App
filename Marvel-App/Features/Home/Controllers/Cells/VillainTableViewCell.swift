87a7d6b90edbd44f45379c3badbffac3f2456d60
seneca <senecamartin11@gmail.com>
2021-07-09 08:47:00 +0200

    App finalizada y funcionando con todos los requisitos

---
+++ Marvel-App/Features/Home/Controllers/Cells/VillainTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  VillainTableViewCell.swift
+//  Marvel-App
+//
+//  Created by seneca on 09/07/2021.
+//
+
+import UIKit
+
+class VillainTableViewCell: UITableViewCell {
+    static let cellIdentifier: String = String(describing: VillainTableViewCell.self)
+    
+    // MARK - IBOutlets
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var villainImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var power: UILabel!
+    
+    
+    // MARK: -Lifecycle
+    
+    // Se llama cuando se crea la celda
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Para que al pulsar la celda no se quede en gris como pulsada
+        selectionStyle = .none
+        
+        cellView.layer.cornerRadius = 4.0
+        cellView.layer.shadowColor = UIColor.gray.cgColor
+        cellView.layer.shadowOffset = CGSize.zero
+        cellView.layer.shadowOpacity = 0.7
+        cellView.layer.shadowRadius = 4.0
+    }
+    
+    // Se utiliza cuando se vuelven a utilizar las celdas que desaparecen en el scroll
+    // Esta funcion si es importante para la reutilización de las celdas
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+    // Limpiamos los datos de las celdas. Es importante
+        villainImage.image = nil
+        name.text = nil
+        power.text = nil
+    }
+    
+    
+    // Funcion para cargar los datos del modelo a los componentes de la celda
+    func updateViews (metahuman: Metahuman) {
+        update(image: metahuman.image)
+        update(name: metahuman.name)
+        update(power: metahuman.power)
+    }
+    
+    
+    private func update(image: String?) {
+        villainImage.image = UIImage(named: image ?? "")
+    }
+    
+    private func update(name: String?) {
+        self.name.text = name
+    }
+    
+    private func update(power: Float?) {
+        self.power.text = "Poder: \(String(describing: power ?? 0))"
+    }
+}
---
+++ Marvel-App/Features/Home/Controllers/VillainTableViewController.swift
@@ -0,0 +1,87 @@
+//
+//  VillainTableViewController.swift
+//  Marvel-App
+//
+//  Created by seneca on 09/07/2021.
+//
+
+import UIKit
+
+class VillainTableViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Private properties
+    private let metaHumanRepository = MetahummanRepository()
+    private var metaHumans: Metahumans = []
+    
+    private let segueToDetail = "SEGUE_FROM_HOME_TO_DETAIL"
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        loadData()
+        tableView.reloadData()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? DetailViewController,
+              let metahumanSelected = sender as? Metahuman else {
+            return
+        }
+        
+        destination.metahuman = metahumanSelected
+    }
+    
+    private func loadData() {
+        metaHumans = metaHumanRepository.metahumansVillain
+    }
+}
+
+extension VillainTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return metaHumans.count
+    }
+    
+    // Esto va a ser así el 90% de las veces
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: VillainTableViewCell.cellIdentifier,
+                                                 for: indexPath) as? VillainTableViewCell
+        
+        // Pintamos los datos de la celda
+        if indexPath.row < metaHumans.count {
+            cell?.updateViews(metahuman: metaHumans[indexPath.row])
+        }
+        
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row < metaHumans.count {
+           let metahuman = metaHumans[indexPath.row]
+            
+            // Navegando con SEGUE
+            performSegue(withIdentifier: segueToDetail,
+                         sender: metahuman)
+            
+            
+            // Para hacerlo sin SEGUE
+//            let storyboardHome = UIStoryboard(name: "Detail",
+//                                              bundle: nil)
+//            if let destination = storyboardHome.instantiateInitialViewController() as? DetailViewController {
+//                destination.metahuman = metahuman
+//                navigationController?.pushViewController(destination,
+//                                                         animated: true)
+//            }
+        }
+    }
+}
